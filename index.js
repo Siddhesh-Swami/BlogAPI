d723b0e32bc617c3a475b39c9679e04f4fa088b0
Siddhesh Swami <Siddhesh.Swami@ril.com>
2020-11-06 19:57:23 +0530

    On routing , add user get all users update user

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// dotenv to hide private apikeys & passwords
 require('dotenv').config()
 
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 var mysql = require('mysql2');
-// To connect to mysql DB
+
+// parse application/json
+app.use(bodyParser.json());
+
+// creating a connection
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -11,23 +17,87 @@ var con = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
+//connect to MYSQL DB
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Connected to the Database Successfully!");
 });
 
-con.query('SELECT * FROM usermodel', function (error, results, fields) {
-  if (error)
-      throw error;
+// select query
 
-  results.forEach(result => {
-      console.log(result);
+// con.query('SELECT * FROM usermodel', function (error, results, fields) {
+//   if (error)
+//       throw error;
+
+//   results.forEach(result => {
+//       console.log(result);
+//   });
+// });
+
+// insert query
+/*
+con.query('INSERT INTO `blog`.`usermodel` (`username`, `email`, `password`, `bio`) VALUES ("sid2", "sid2@gmail.com", "qwerty", "coder")', function (error, results, fields) {
+    if (error)
+        throw error;
+    else
+        console.log("record inserted successfully");    
+  
+    results.forEach(result => {
+        console.log(result);
+    });
   });
-});
+*/
 
 app.get("/", function(req,res){
 	res.send("Hello World");
 });
+
+//GET ALL USERS DATA
+app.get("/api/users", function(req,res){
+    let sql = 'SELECT * FROM usermodel';
+    let query = con.query(sql, function(err, results){
+        if(err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+});
+
+//GET USER BY USERNAME
+
+app.get('/api/users/:username',(req, res) => {
+    let sql = "SELECT * FROM usermodel WHERE username="+" '"+req.params.username + "' ";
+    console.log(sql);
+    let query = con.query(sql, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+  });
+
+// CREATE USER & ADD TO DATABASE
+// REGISTRATION
+
+app.post("/api/users", function(req,res){
+    let data = {username : req.body.username,
+    email : req.body.email,
+    password: req.body.password,
+    bio : req.body.bio
+    };
+    let sql = "INSERT INTO usermodel SET ?";
+    let query = con.query(sql, data,(err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      });
+})
+
+//UPDATE USER
+
+app.put('/api/users/:username',(req, res) => {
+    let sql = "UPDATE usermodel SET password='"+req.body.password+"', email='"+req.body.email+"' WHERE username="+ " '"+ req.params.username + "' ";
+    console.log(sql);
+    let query = con.query(sql, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+  });
 
 app.listen(3000, function(){
 	console.log("Server is running on port 3000.");
