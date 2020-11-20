46cd1a76d766622f4228f03fa2cce05c3e804b1f
Siddhesh Swami <Siddhesh.Swami@ril.com>
2020-11-20 14:04:52 +0530

    Create Article & Get Article implemented

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,15 +1,59 @@
+const SequelizeSlugify = require('sequelize-slugify')
 const {Users, Articles} = require('../models');
 
-//to complete
-async function createArticle(options){
-    if (!options.slug) {
-        throw new Error('Did not supply slug')
-      }
-      if (!options.title) {
-        throw new Error('Did not supply title')
-      }
 
+async function createArticle(articleOpts,username) {
+  if (!articleOpts.title) {
+    throw new Error('Did not supply title')
+  }
+ 
+  SequelizeSlugify.slugifyModel(Articles, {
+    source: ['title']
+  });
 
-
-
+  if (!articleOpts.description) {
+    throw new Error('Did not supply description')
+  }
+  if (!articleOpts.body) {
+    throw new Error('Did not supply body')
+  }
+ 
+  const article = await Articles.create({
+    ...articleOpts, 
+    authorUsername:username,
+    userUsername: username
+  })
+ 
+  if (!article) {
+    throw new Error('Error creating article')
+  }
+ 
+  const createdArticle = await Articles.findOne({
+    attributes: ['title', 'description', 'body'],
+    where: {
+      title: article.title
+    }
+  })
+ 
+  return {
+    ...createdArticle.get(),
+    
+  }
 }
+
+async function getArticle(slug){
+  const fetchedArticle = await Articles.findOne({
+    attributes: ['slug', 'title', 'description', 'body','createdAt','updatedAt'],
+      where : {
+          slug : slug
+      },
+      include: [{model:Users, as:'author',attributes:['username','bio','image'] }]
+ 
+  })
+ 
+  console.log(fetchedArticle)
+  return fetchedArticle
+
+} 
+
+module.exports = {createArticle, getArticle}
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,22 +1,38 @@
 var router = require('express').Router();
 const {authenticateToken} = require("../../middlewares/auth")
+const {createArticle,getArticle}=require('../../controllers/articles')
 
-//create a post
-//to complete
-router.post('/articles', authenticateToken ,function(req,res){
+//To Create an Article
+router.post('/articles', authenticateToken , async function(req,res){
     console.log(req.body.article);
-
-    createArticle(req.body.article)
-    /*
+    if(req.user)
     {
-        "article": {
-          "title": "How to train your dragon",
-          "description": "Ever wonder how?",
-          "body": "You have to believe"
-        }
+      try {
+        const createdArticle= await createArticle(req.body.article,req.user.username);
+        res.send(createdArticle);
+      } catch (err) {
+        res.status(403).send({
+          errors: {
+            body: [ err.message ]
+          }
+        })
       }
-      */
+    }
+       
 })
 
+//To get an Article
+router.get('/articles/:slug' , async function(req,res){
+  try {
+    const fetchedArticle= await getArticle(req.params.slug);
+    res.send(fetchedArticle);
+  } catch (err) {
+    res.status(403).send({
+      errors: {
+        body: [ err.message ]
+      }
+    })
+  }
+})
 
 module.exports = router;
